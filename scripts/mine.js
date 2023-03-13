b7b052888ba33950af98e46353ded719654cf73b
Conor Getty <conorgetty@gmail.com>
2023-03-13 15:55:23 -0500

    end of day. wanted to save work

--- scripts/mine.js
+++ scripts/mine.js
@@ -1,12 +1,55 @@
-import { getMines } from "./database.js"
+import { getMines, getMinerals } from "./database.js"
 
 const mines = getMines()
 
+let selectedMine = null
+
+document.addEventListener(
+       "change",
+       (changeEvent) => {
+              if (changeEvent.target.id === "mines"){
+                     const selectedMineId = parseInt(changeEvent.target.value)
+                     for (const mine of mines) {
+                            if (mine.id === selectedMineId){
+                                   selectedMine = {...mine}
+                            }
+                     }
+                     checkSelectedMine(selectedMine, mineMinerals)
+              }
+       }
+)
+
+export const checkSelectedMine = (selectedMine, mineMinerals) => {
+       let html = "<ul>"
+       let selectedMineMinerals = []
+
+       const minerals = getMinerals()
+       const mineMinerals = getAvailableMinerals()
+
+       //find matching mineMineral entry based on mine
+       for (const mineMineral of mineMinerals){
+              if (mineMineral.mineId === selectedMine.id){
+                     selectedMineMinerals.push(mineMineral)
+              }
+       }
+
+              const listItems = minerals.map(mineral => {
+                     for (const selectedMineMineral of selectedMineMinerals){
+                            if (mineral.id === selectedMineMineral.mineralId){
+                                   return `<li>
+                                          <input type="radio" name="mineral" value="${mineral.id}" /> 'mineral amount' tons of ${mineral.name}
+                                   </li>`
+                            }
+                     }
+              })
+       html += listItems.join("")
+       html += "</ul>"
+
+       return html
+}
+
 export const Mines =() =>{
     let html=""
-    
-    
-    
     
     html += `<select name="mines" id="mines">`
     
@@ -23,8 +66,4 @@ export const Mines =() =>{
            html += "</select>"
     
            return html
-           
-    
-        
-    
-    }
+       }
