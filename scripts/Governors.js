c86049afb82646382932d795da0b5b79ba19ac90
Michael Stewart <michaelpstewart@comcast.net>
2023-03-13 11:53:10 -0500

    governors display2

--- scripts/Governors.js
+++ scripts/Governors.js
@@ -7,10 +7,13 @@ export const Governors = () => {
     let html = `<select id="resource">`
     html += `<option value="0">Select Governor</option>`
     let governorsList = governors.map((governor) => {
-        return `<option value="${governor.id}>${governor.name}</option>`
+        if (governor.active === true) {
+            return `<option value="${governor.id}>${governor.name}</option>`
+        }
     })
     html += governorsList.join('')
     html += "</select>"
     return html
 }
 
+//add event listener for setting the state
