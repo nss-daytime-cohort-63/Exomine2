9dcc37cac8eb94ecbf98ef7490375950c0138ab5
Dan Brady <dannymb360@gmail.com>
2023-03-13 16:12:44 -0400

    orderHistory

---
+++ scripts/orderH.js
@@ -0,0 +1,30 @@
+import { getMinerals } from "./database.js"
+import { getBoughtMinerals } from "./database.js"
+
+const minerals = getMinerals()
+const boughtMinerals =getBoughtMinerals()
+
+
+export const orderHistory =()=>{
+
+for (const mineral of minerals){
+    for(const boughtMineral of boughtMinerals){
+        if(mineral.id === boughtMineral.mineralId){
+            let html =`<li>${mineral.mineralAmount} ${mineral.name}</li>`
+            return html
+        }
+    }
+}
+
+}
+
+
+document.addEventListener(
+    "click",
+    (clickEvent)=>{
+        const itemClicked =clickEvent
+        if(itemClicked.id ==="orderButton"){
+            orderHistory()
+        }
+    }
+)
