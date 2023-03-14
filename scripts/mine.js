f34a7ea51c84f0eb795ef1658763ab37229c53ea
Conor Getty <conorgetty@gmail.com>
2023-03-14 09:10:59 -0500

    modularized code. still need to make minor changes

--- scripts/mine.js
+++ scripts/mine.js
@@ -1,4 +1,4 @@
-import { getMines, getMinerals } from "./database.js"
+import { getMines, getMinerals, getAvailableMinerals } from "./database.js"
 
 const mines = getMines()
 
@@ -7,63 +7,68 @@ let selectedMine = null
 document.addEventListener(
        "change",
        (changeEvent) => {
-              if (changeEvent.target.id === "mines"){
+              if (changeEvent.target.id === "mines") {
                      const selectedMineId = parseInt(changeEvent.target.value)
                      for (const mine of mines) {
-                            if (mine.id === selectedMineId){
-                                   selectedMine = {...mine}
+                            if (mine.id === selectedMineId) {
+                                   selectedMine = mine
                             }
                      }
-                     checkSelectedMine(selectedMine, mineMinerals)
               }
        }
 )
 
-export const checkSelectedMine = (selectedMine, mineMinerals) => {
-       let html = "<ul>"
+const findMatchingMineMinerals = (selectedMine) => {
        let selectedMineMinerals = []
-
-       const minerals = getMinerals()
        const mineMinerals = getAvailableMinerals()
 
-       //find matching mineMineral entry based on mine
-       for (const mineMineral of mineMinerals){
-              if (mineMineral.mineId === selectedMine.id){
+       for (const mineMineral of mineMinerals) {
+              if (mineMineral.mineId === selectedMine.id) {
                      selectedMineMinerals.push(mineMineral)
               }
        }
+       return selectedMineMinerals
+}
 
-              const listItems = minerals.map(mineral => {
-                     for (const selectedMineMineral of selectedMineMinerals){
-                            if (mineral.id === selectedMineMineral.mineralId){
-                                   return `<li>
-                                          <input type="radio" name="mineral" value="${mineral.id}" /> 'mineral amount' tons of ${mineral.name}
+export const Mines = () => {
+       let html = ""
+
+       html += `<select name="mines" id="mines">`
+
+       html += `<option value="0">Select a Mine Facility</option>`
+
+       const arrayMines = mines.map((mine) => {
+              if (mine.active === true) {
+                     return `<option value="${mine.id}" name ="mine">${mine.name}</option>`
+              }
+       }
+       )
+
+       html += arrayMines.join(``)
+       html += "</select>"
+
+       return html
+}
+
+const selectedMineMinerals = findMatchingMineMinerals(selectedMine)
+
+//invoke buildMineralOptions in exominer.js
+export const buildMineralOptions = () => {
+       let html = "<ul>"
+
+       const minerals = getMinerals()
+
+       const listItems = minerals.map(mineral => {
+              for (const selectedMineMineral of selectedMineMinerals) {
+                     if (mineral.id === selectedMineMineral.mineralId) {
+                            return `<li>
+                                          <input type="radio" name="mineral" value="${mineral.id}" /> ${selectedMineMineral.mineralAmount} tons of ${mineral.name}
                                    </li>`
-                            }
                      }
-              })
+              }
+       })
        html += listItems.join("")
        html += "</ul>"
 
        return html
 }
-
-export const Mines =() =>{
-    let html=""
-    
-    html += `<select name="mines" id="mines">`
-    
-    html += `<option value="0">Select a Mine Facility</option>`
-           
-           const arrayMines = mines.map( (mine) =>{
-            if(mine.active === true){
-           return `<option value="${mine.id}" name ="mine">${mine.name}</option>`
-            }
-           }
-           )
-           
-           html +=  arrayMines.join(``)
-           html += "</select>"
-    
-           return html
-       }
