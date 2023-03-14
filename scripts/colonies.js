82ade8b5caa1aa1fbac3d1afd4570614917eefb3
John Brady <johntbrady01@gmail.com>
2023-03-14 15:10:52 -0400

    colonie

--- scripts/colonies.js
+++ scripts/colonies.js
@@ -20,3 +20,44 @@ document.addEventListener(
            }
     }
 )
+
+/*
+import { getGovernors } from "./database.js";
+import { getColonies } from "./database.js"
+import { getTransientState } from "./database.js";
+import { getTransientStateColony } from "./database.js";
+
+
+
+export const colonyDisplay = () => {
+    let html = '<h2>Colony Display</h2>'
+    
+    //get the current state for selected colony 
+    //if you find a governor/colony display that data
+    //if you dont find a governor/colony display the default colony display
+    
+    const colonies = getColonies()
+    const govs = getGovernors()
+    const tranColony = getTransientStateColony()
+
+    const transientState = getTransientState()
+
+   const govFound=govs.find((gov) =>  {
+        return gov.id === transientState.selectedGovernor
+   
+})
+    
+    const colonyFound=colonies.find((colony) => {
+
+        return colony.id=== transientState.selectedColony.id
+    })
+
+    if(govFound.colonyId===colonyFound.id){
+        
+        html=`<h2>${colonyFound.name} display</h2>`
+        return html
+    }
+    else{
+        return html
+    }
+} */
